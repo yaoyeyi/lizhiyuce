1155551f3367d7dee945fd8c230355830c373cdc
yaoyeyi <yeweinai@qq.com>
2018-05-17 07:29:50 +0800

    更新

---
+++ untitled1.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 10 16:55:01 2018
+
+@author: Weizh
+"""
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as ans
+
+filename = 'HR_comma_sep.csv'
+data = pd.read_csv(filename)
+print(data.head())
